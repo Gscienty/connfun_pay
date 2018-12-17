73f64d03d83979e430ce02ec2041f24c0ca00d19
Gscienty <gaoxiaochuan@hotmail.com>
2018-12-17 11:19:44 +0800

    add wechat req/res entity metadata

---
+++ src/main/java/com/connfun/pay/wechat/common/EntityName.java
@@ -0,0 +1,13 @@
+package com.connfun.pay.wechat.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD })
+public @interface EntityName {
+    String value();
+}
+
