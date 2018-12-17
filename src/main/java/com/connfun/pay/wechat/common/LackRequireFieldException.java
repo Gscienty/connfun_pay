7b1dc2528ae2c34b2f83588c1005c342a0bdd519
Gscienty <gaoxiaochuan@hotmail.com>
2018-12-17 11:22:48 +0800

    add Exception

---
+++ src/main/java/com/connfun/pay/wechat/common/LackRequireFieldException.java
@@ -0,0 +1,10 @@
+package com.connfun.pay.wechat.common;
+
+public class LackRequireFieldException extends Exception {
+    private static final long serialVersionUID = 1245356L;
+
+    public LackRequireFieldException() {
+        super();
+    }
+}
+
